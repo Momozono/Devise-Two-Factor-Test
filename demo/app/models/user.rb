b4b6a24f299f7edca9395e578998b1b4f270e075
Junya Namatame <tanatana1089@gmail.com>
2015-09-18 12:47:02 +0900

    最低限の動作確認を完了

--- demo/app/models/user.rb
+++ demo/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   devise :two_factor_authenticatable,
+         :otp_secret_encryption_key => ENV['ENCRYPTION_KEY']
+
+  devise :two_factor_authenticatable,
          :otp_secret_encryption_key => ENV['ENCRYPTION_KEY']  # Set a unique encryption key for your app.
                                                                         # Store your key as an ENV variable and
                                                                         # remember to add it to .gitignore 
